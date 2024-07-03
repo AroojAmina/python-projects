12a674a07bdfa5551317a8d3948b035a80f8f9ca
AroojAmina <167509324+AroojAmina@users.noreply.github.com>
2024-07-03 02:29:45 -0700

    Update typingspeedcalculator.py

--- typingspeedcalculator.py
+++ typingspeedcalculator.py
@@ -1,38 +1,38 @@
-from time import *
+from time import time
 import random as r
-def mistake(partest, usertest) :
+
+def mistake(partest, usertest):
     error = 0
     for i in range(len(partest)):
         try:
             if partest[i] != usertest[i]:
-                error = error + 1
+                error += 1
         except:
-            error = error + 1
+            error += 1
     return error
+
 def speed_time(time_s, time_e, userinput):
     time_delay = time_e - time_s
     time_R = round(time_delay, 2)
-    speed = len(userinput)/ time_R
+    speed = len(userinput) / time_R
     return round(speed)
+
 while True:
-    ck = input(" ready to test: yes / no: ")
+    ck = input("Ready to test: yes / no: ").strip().lower()
     if ck == "yes":
-        test = "HI I Am Techpixel", "welcome to my site"
-    test1 = r.choice(test)
-    print("***** Typing Speed *****")
-    print(test1)
-    print()
-    print()
-    time_1 = time()
-    testinput = input("Enter :")
-    time_2 = time()
-    print('speed: ', speed_time(time_1, time_2, testinput), "w/sec")
-    print("error:", mistake(test1, testinput))
-
-
-    print(" thank you ")
-else:
-    print(" wrong input")
-
-
-
+        test = ("HI I Am Techpixel", "welcome to my site")
+        test1 = r.choice(test)
+        print("***** Typing Speed *****")
+        print(test1)
+        print()
+        time_1 = time()
+        testinput = input("Enter: ")
+        time_2 = time()
+        print('Speed:', speed_time(time_1, time_2, testinput), "w/sec")
+        print("Error:", mistake(test1, testinput))
+        print("Thank you")
+    elif ck == "no":
+        print(" thank you. ")
+        break
+    else:
+        print("Wrong input. Please enter 'yes' or 'no'.")
